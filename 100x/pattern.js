c785ca6afd87ac954e1acdac8b3344e548a017aa
devil5251069 <102917051+devil5251069@users.noreply.github.com>
2023-08-12 00:39:29 +0530

    Learned animations.js

--- 100x/pattern.js
+++ 100x/pattern.js
@@ -7,4 +7,30 @@ function pattern(n){
     }
 }
 
-pattern(5)
+function starPattern(n){
+    for(let row = 1; row <= n; row++){
+        for(let col = 1; col <= 2*row ; col++){
+            process.stdout.write("*" + " ");
+        }
+        console.log()
+    }
+}
+function starPattern2(n){
+    for(let row = 1; row <= n; row++){
+        for(let col = 1; col <= 2*row - 1 ; col++){
+            process.stdout.write("*" + " ");
+        }
+        console.log()
+    }
+}
+
+function complex(n){
+    let sum = 0
+    for (let index = 0; index <= n; index++) {
+        sum += index
+        
+    }
+    return sum
+}
+
+// starPattern2(5)
