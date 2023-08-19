033a73c79b895f648e1adfb4621b479015b594de
Aditya Bhave~ <adityabhave525@gmail.com>
2023-08-20 00:06:29 +0530

    completed pong.js

--- index.js
+++ index.js
@@ -13,6 +13,8 @@ const bellBorderColor = "black"
 const ballRadius = 12.5
 const paddleSpeed = 50
 let intervalId
+let ballColor = "yellow"
+let ballBorderColor = "black"
 let ballSpeed = 1
 let ballX = gameWidth / 2
 let ballY = gameHeight / 2
@@ -71,10 +73,27 @@ function drawPaddles(){
 
 }
 function createBall(){
-    
+    ballSpeed = 1
+    if(Math.round(Math.random()) == 1){
+        ballXDirection = 1
+    }
+    else{
+        ballXDirection = -1
+    }
+    if(Math.round(Math.random()) == 1){
+        ballYDirection = 1
+    }
+    else{
+        ballYDirection = -1
+    }
+    ballX = gameWidth / 2
+    ballY = gameHeight / 2
+
+    drawBall(ballX, ballY)
 }
 function moveBall(){
-    
+    ballX += (ballSpeed * ballXDirection)
+    ballY += (ballSpeed * ballYDirection)
 }
 function drawBall(ballX, ballY){
     ctx.fillStyle = ballColor
@@ -86,8 +105,44 @@ function drawBall(ballX, ballY){
     ctx.fill()
 }
 function checkCollision(){
-    
+    if(ballY <= 0 + ballRadius){
+        ballYDirection *= -1
+    }
+    if(ballY >= gameHeight - ballRadius){
+        ballYDirection *= -1
+
+    }
+    if(ballX <= 0){
+        player2Score += 1
+        updateScore()
+        createBall()
+        return
+    }
+    if(ballX >= gameWidth){
+        player1Score += 1
+        updateScore()
+        createBall()
+        return
+    }
+
+    if(ballX <= (paddle1.x + paddle1.width + ballRadius)){
+        if(ballY >= paddle1.y && ballY < paddle1.y + paddle1.height){
+            ballX = (paddle1.x + paddle1.width + ballRadius) // if ball stuck
+            ballXDirection *= -1
+            ballSpeed += 1
+        }
+    }
+
+    if(ballX >= (paddle2.x - ballRadius)){
+        if(ballY > paddle2.y && ballY < paddle2.y + paddle2.height){
+            ballX = (paddle2.x - ballRadius) // if ball stuck
+
+            ballXDirection *= -1
+            ballSpeed += 1
+        }
+    }
 }
+
 function changeDirection(){
     const keyPressed = event.keyCode
     const paddle1Up = 87
@@ -120,8 +175,29 @@ function changeDirection(){
         }
 }
 function updateScore(){
-    
+    scoreText.textContent = `${player1Score} : ${player2Score}`
 }
 function resetGame(){
-
+    player1Score = 0
+    player2Score = 0
+    paddle1 = {
+        width : 25,
+        height : 100,
+        x:0,
+        y:0
+    }
+    paddle2 = {
+        width : 25,
+        height : 100,
+        x:gameWidth - 25,
+        y:gameHeight - 100
+    }
+    ballSpeed = 1
+    ballX = 0
+    ballY = 0
+    ballXDirection = 0
+    ballYDirection = 0
+    updateScore()
+    clearInterval(intervalId)
+    gameStart()
 }
